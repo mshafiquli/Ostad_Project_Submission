7448ee13db8338c0ef4e32452e424175e37e499f
Shafiq <shafiq.pmo@bulipetech.com>
2025-11-10 11:30:03 +0600

    modified in task2 file

--- Task2.php
+++ Task2.php
@@ -43,21 +43,23 @@ $result= array_merge($numbers, $numbers1);       //array_merge merges 2 arrays t
 print_r($result);
 echo "</pre>";
 
-echo "<br><h4>Array Sum:</h4> <pre>";
+echo "<br><h4>Array Sum:</h4>";
 $result= array_sum($numbers);       //array_sum add all elements of the array
 print_r($result);
-echo "</pre>";
 
-echo "<p><h2>............3. Explode and Implode............</h2></p><br>";
+
+echo "<p><h2>............3. Explode and Implode............</h2></p>";
 echo "<br><h4>String to Array using Explode:</h4> <pre>";
 $strNames="Sultan Sheikh, Ahmed Badawood, Anowar Hossain, Pervez Ali, Moniruzzaman Chowdhury";
 $arrNames= explode(',', $strNames);       //Explode splits the string into array with specific delimeter
 print_r($arrNames);
-echo "</pre>";
 
-echo "<br><h4>Array to String using Implode:</h4> <pre>";
+
+echo "<br><h4>Array to String using Implode:</h4>";
 $strNames=implode(',', $arrNames);       //Implode joins the array elements with a delimeter in it
 echo $strNames;
+
+
 
 echo "<br><p><h2>............4. String functions strtoupper, strlen, substr, and str_replace............</h2></p>";
 $str='life is beautiful';
@@ -66,6 +68,29 @@ echo '<br>Length of the string : ' . strlen($str);
 echo '<br>Sub String of the string : ' . substr($str,8, 6);     //Display 6 characters from 8th Character of the string
 $newStr=str_replace('beautiful','tough',$str);
 echo '<br>String replace : ' . $newStr;
+
+
+echo "<br><p><h2>............5. Create a file and enter expenses detail............</h2></p>";
+
+echo "<br><h4>Writing in a File:</h4>";
+$file= fopen('expenses.txt','w');
+fwrite($file, "\nExpense Detail\n...............\nFood : 20000, \nTransport: 15000, \nOthers: 50000");
+fwrite($file, "\nHouse Rent: 25000");
+echo "<pre>";
+echo file_get_contents('expenses.txt');
+echo "</pre>";
+fclose($file);
+
+
+echo "<br><h4>Appending in a File:</h4>";
+$file=fopen('expenses.txt','a');
+fwrite($file, "\n---------------------");
+fwrite($file, "\nTotal Expenses: 110000");
+echo "<pre>";
+echo file_get_contents('expenses.txt');
+echo "</pre>";
+fclose($file);
+
 
 
 ?>
